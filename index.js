00548ff2fe081ed85676daa97cfbef202be8cdf6
Denis <shishkindv@gmail.com>
2021-07-23 11:08:12 +0300

    fix

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import TelegramBot = require('node-telegaram-bot-api')
-const token = '1840698273:AAFZcoOhDbwhzUParZLIv6JO52GLwwB-XLA'
+process.env.NTBA_FIX_319 = 1;
+const TelegramBot = require('node-telegram-bot-api')
+const TOKEN = '1840698273:AAFZcoOhDbwhzUParZLIv6JO52GLwwB-XLA'
 
-const bot = new TelegramBot(TOKEN, {polling: true})
+const bot = new TelegramBot(TOKEN, {
+    polling: true
+})
+bot.on("polling_error", console.log);
 
 bot.on('message', msg => {
-    bot.sendMeassage(msg.chat.id, `Hello from Heroku, ${msg.from.first_name}`)
+    bot.sendMessage(msg.chat.id, `Hello from Heroku, ${msg.from.first_name}`)
 })
