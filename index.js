a4418f4d0066e7d52d5729b41d347c6b4e0d3a5b
Denis <shishkindv@gmail.com>
2021-07-23 15:45:34 +0300

    require('https').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){   res.end('') });

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-process.env.NTBA_FIX_319 = 1;
+//process.env.NTBA_FIX_319 = 1;
+require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
+    res.end('')
+  });
 const TelegramBot = require('node-telegram-bot-api')
 const TOKEN = '1840698273:AAFZcoOhDbwhzUParZLIv6JO52GLwwB-XLA'
 
